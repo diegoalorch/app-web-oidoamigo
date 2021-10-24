09b83bd65c4f9e97da44ca059454a07a9cd61565
Diego Alor <diegoalor@upeu.edu.pe>
2021-10-23 20:03:59 -0500

    frontend listo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AgregarPsicologoComponent } from './components/agregar-psicologo/agregar-psicologo.component';
+import { ConfiguracionComponent } from './components/configuracion/configuracion.component';
+import { ListaPacientesComponent } from './components/lista-pacientes/lista-pacientes.component';
+import { ListaPsicologosComponent } from './components/lista-psicologos/lista-psicologos.component';
 import { ModulosComponent } from './components/modulos/modulos.component';
 import { PacienteAsignadoComponent } from './components/paciente-asignado/paciente-asignado.component';
 import { PacienteConsultaComponent } from './components/paciente-consulta/paciente-consulta.component';
+import { ReportePacientesComponent } from './components/reporte-pacientes/reporte-pacientes.component';
 import { LoginComponent } from './login/login.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 
+const routesHome: Routes = [
+
+  {path : '', component: PacienteConsultaComponent}, //paciente consulta
+  {path : 'sidebar', component: SidebarComponent},
+  {path : 'login', component: LoginComponent }, //login
+  {path : 'modulos', component: ModulosComponent}, //modulos
+  {path : 'agregar-psicologo', component: AgregarPsicologoComponent}, //agregar psicologo
+  {path : 'configuracion', component: ConfiguracionComponent}, //configuracion
+  {path : 'reporte-paciente', component: ReportePacientesComponent}, //reporte paciente
+  {path : 'lista-psicologos', component: ListaPsicologosComponent}, //lista de psicologos
+  {path : 'lista-pacientes', component: ListaPacientesComponent}, //lista de pacientes
+  {path : 'paciente-asignado', component: PacienteAsignadoComponent}, //paciente asiganado
+];
+
 const routes: Routes = [
 
-  {path : 'sidebar', component: SidebarComponent },
-  {path : 'login', component: LoginComponent },
-  {path : 'modulos', component: ModulosComponent},
-  {path : '', component: PacienteConsultaComponent},
-  {path : 'paciente-asignado', component: PacienteAsignadoComponent}
+  {path : 'login', component: LoginComponent }, //login
+  {path : 'sidebar', component: SidebarComponent, children: routesHome }, //sidebar
+  {path : 'modulos', component: ModulosComponent}, //modulos
+  {path : '', component: PacienteConsultaComponent}, //paciente consulta
 ];
 
 @NgModule({
@@ -20,3 +38,9 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routingComponents = [
+  SidebarComponent,
+  LoginComponent,
+  ModulosComponent
+];
--- src/app/components/paciente-consulta/paciente-consulta.component.ts
+++ src/app/components/paciente-consulta/paciente-consulta.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
+import 'animate.css';
 
 @Component({
   selector: 'app-paciente-consulta',
@@ -11,5 +13,26 @@ export class PacienteConsultaComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  
+  registrarconsulta(){
+    console.log("Funciono bien")
+    Swal.fire({
+      title: '<b style="color: #000000; font-family: Poppins, sans-serif; font-weight: 900; font-size: 45px">Registro Exitoso</b>',
+      // showClass: {
+      //   popup: 'animate__animated animate__fadeInDown'
+      // },
+      // hideClass: {
+      //   popup: 'animate__animated animate__fadeOutUp'
+      // },
+      icon: 'success',
+      html: '<span style="font-size: 22px">Se ha Registrado tú Consulta, lo contactaremos en breve...!</span>',
+      // imageUrl: 'https://unsplash.it/400/200',
+      // imageWidth: 400,
+      // imageHeight: 200,
+      // imageAlt: 'Custom image',
+      confirmButtonText: 'Aceptar',
+      confirmButtonColor: '#52A820',
+      iconColor: '#52A820'
+    })
+  }
 }
